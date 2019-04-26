058a21a462a799b7bba4fce45b96087807022783
KITSAndriiBorovyi <Andrii_Borovyi@epam.com>
2019-04-26 16:42:59 +0300

    add allure

--- src/test/java/stepsDefinition/Steps.java
+++ src/test/java/stepsDefinition/Steps.java
@@ -2,30 +2,35 @@ package stepsDefinition;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static utils.MyDriverManager.initChromeDriver;
 
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 
 public class Steps {
 
+    @Step
     @Given("^Open the Chrome$")
     public void open_the_Firefox_and_launch_the_application() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebDriverRunner.setWebDriver(initChromeDriver());
-//        System.setProperty("webdriver.chrome.driver",
-//                "src/drivers/chromedriver.exe");
-//        System.setProperty("selenide.browser", "Chrome");
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        WebDriverRunner.setWebDriver(initChromeDriver());
+        System.setProperty("webdriver.chrome.driver",
+                "src/drivers/chromedriver.exe");
+        System.setProperty("selenide.browser", "Chrome");
     }
 
+    @Step
     @When("^Open web page$")
     public void enter_the_Username_and_Password() {
         open("https://www.epam.com/");
@@ -35,5 +40,12 @@ public class Steps {
     public void reset_the_credential() {
         String text = $(By.xpath(".//a[contains(@class, 'cta-button-ui')]")).getText();
         Assert.assertEquals(text, "CONTACT US");
+        makeScreenShot();
+    }
+
+    @Attachment
+    @Step("Make screen shot of results page")
+    public byte[] makeScreenShot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
